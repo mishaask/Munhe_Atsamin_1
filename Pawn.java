dacd224b0c18b345f066b1ea1e3983170b1dbe56
mishaask <122132689+mishaask@users.noreply.github.com>
2024-01-31 04:34:05 +0000

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,14 +1,31 @@
 public class Pawn extends ConcretePiece{
 
+    public Pawn(Player pieceOwner,Position position,int number){
 
-    public Pawn(Player pieceOwner){
         this.pieceOwner = pieceOwner;
+        this.position = position;
 
+    }
+    public Pawn(Player pieceOwner,Position position){
+
+        this.pieceOwner = pieceOwner;
+        this.position = position;
+        this.setPosition(position);
+        if (this.getOwner().isPlayerOne())
+            {this.pieceString= "D";}
+        else {this.pieceString= "A";}
     }
 
     @Override
     public Player getOwner() {
         return pieceOwner;
+    }
+    public Position getPos() {
+        return position;
+    }
+    @Override
+    public String getString() {
+    return pieceString;
     }
 
     @Override
